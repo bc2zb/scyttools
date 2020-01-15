0231a3ad072470542528a57eb1873e86ecaa177c
Brian Capaldo <Brian.Capaldo@gmail.com>
2020-01-15 09:10:37 -0500

    developing glmpca integration

---
+++ glmpca-integration-dev.R
@@ -0,0 +1,9 @@
+source("scyttools_functions.R")
+
+load("/Volumes/Group05/CCBB/Single-Cell-Bioinformatics-2019-October-03/SCAF1121_35_2A_CTL/qc-sce.Rdata")
+
+
+load("/Volumes/Group05/CCBB/Single-Cell-Bioinformatics-2019-October-03/SCAF1122_35_3A_CTL/qc-sce.Rdata")
+
+
+load("/Volumes/Group05/CCBB/Single-Cell-Bioinformatics-2019-October-03/SCAF1153_CT35_2_CTL/qc-sce.Rdata")
